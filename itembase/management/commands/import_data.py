1129b00e73129941e7e0d4d11816592cd08ed615
Nurassyl Zekenov <nzekenov@alumni.cmu.edu>
2023-10-28 02:52:50 +0600

    Added simple parent-child approach, but failing to find parents for orphan

--- itembase/management/commands/import_data.py
+++ itembase/management/commands/import_data.py
@@ -3,56 +3,45 @@ import os
 import pandas as pd
 from itembase.models import Item
 
-
 class Command(BaseCommand):
     help = 'Imports data from Excel into the database'
-
-    def build_hierarchy(self, code):
-        """Generate a list of hierarchical codes for the given code."""
-        hierarchy = []
-        for i in range(1, len(code) + 1):
-            hierarchy.append(code[:i])
-        return hierarchy
 
     def handle(self, *args, **kwargs):
         file_path = 'data/file.xlsx'
 
+        # Ensure the file exists
         if not os.path.exists(file_path):
             self.stdout.write(self.style.ERROR(f"File {file_path} does not exist."))
             return
 
+        # Load the Excel data
         xls = pd.ExcelFile(file_path)
         df_list1 = xls.parse("Лист1")
-        df_list1["Номер схемы"] = df_list1["Номер схемы"].astype(str).str.split().str[0].str.strip()
-        df_list1["Чертежный номер"] = df_list1["Чертежный номер"].astype(str)
 
-        # Start with root nodes
-        root_codes = ["PJ350200000", "PJ350150000", "PJ350170000"]
-        items = {root_code: Item.objects.get_or_create(code=root_code)[0] for root_code in root_codes}
+        # Split the values in column C by comma and take the first part
+        df_list1["Номер схемы"] = df_list1["Номер схемы"].str.split().str[0].str.strip()
 
-        all_codes = set(df_list1["Номер схемы"]).union(set(df_list1["Чертежный номер"]))
+        # Populate the Django model
+        items = {}
+        for _, row in df_list1.iterrows():
+            parent_code, child_code = row["Номер схемы"], row["Чертежный номер"]
+            if parent_code not in items:
+                parent_item, _ = Item.objects.get_or_create(code=parent_code)
+                parent_item.scheme_name = row["Наименование схемы"]
+                parent_item.name_russian = row["Название на русском"]
+                parent_item.name_chinese = row["Название на китайском"]
+                parent_item.quantity = row["Количество"]
+                parent_item.save()
+                items[parent_code] = parent_item
 
-        for code in all_codes:
-            hierarchy = self.build_hierarchy(code)
-            parent_item = None
-
-            for segment in hierarchy:
-                # If this segment is not already in the dictionary, create it
-                if segment not in items:
-                    current_item = Item.objects.create(code=segment, parent=parent_item)
-                    items[segment] = current_item
-                else:
-                    current_item = items[segment]
-
-                # Update details only for the full code (last segment of hierarchy)
-                if segment == code:
-                    matching_rows = df_list1[df_list1["Чертежный номер"] == code]
-                    if not matching_rows.empty:
-                        current_item.scheme_name = matching_rows.iloc[0]["Наименование схемы"]
-                        current_item.name_russian = matching_rows.iloc[0]["Название на русском"]
-                        current_item.name_chinese = matching_rows.iloc[0]["Название на китайском"]
-                        current_item.save()
-
-                parent_item = current_item
+            if child_code not in items:
+                child_item, _ = Item.objects.get_or_create(code=child_code)
+                child_item.scheme_name = row["Наименование схемы"]
+                child_item.name_russian = row["Название на русском"]
+                child_item.name_chinese = row["Название на китайском"]
+                child_item.quantity = row["Количество"]
+                child_item.parent = items[parent_code]
+                child_item.save()
+                items[child_code] = child_item
 
         self.stdout.write(self.style.SUCCESS(f"Successfully imported data from {file_path} into the Item model!"))
